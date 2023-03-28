4cafb2425d25dfdfc106901007abfe47f03c2446
ruilinqi <ruilinqi7@gmail.com>
2023-03-27 23:23:04 -0400

    Add navbar

---
+++ next01/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function Navbar() {
+  return (
+    <nav className="">
+      <h1 className="">
+        <Link href="/">WikiRocket!</Link>
+      </h1>
+    </nav>
+  )
+}
--- next01/app/page.tsx
+++ next01/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+// import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
+    <main className={inter.className}>
+      <h1>Hello</h1>
     </main>
   )
 }
