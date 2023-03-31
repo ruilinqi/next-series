337faef52ba85c2962cb45887f22370a1b882aa2
ruilinqi <ruilinqi7@gmail.com>
2023-03-30 23:55:59 -0400

    Add SearchResults async function and display results

--- next01/app/[searchTerm]/page.tsx
+++ next01/app/[searchTerm]/page.tsx
@@ -1,10 +1,25 @@
+import getWikiResults from "../../lib/getWikiResults"
+
 type Props = {
   params: {
     searchTerm: string
   }
 }
-export default function page({params: {searchTerm}}: Props) {
+export default async function SearchResults({params: {searchTerm}}: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+  const data = await wikiData
+  const results: Result[] | undefined = data?.query?.pages
+  const content = (
+    <main>
+      {results
+        ? Object.values(results).map(result => {
+          return <p>{JSON.stringify(result)}</p>
+        })
+        : <h2>`${searchTerm}`</h2>
+      }
+    </main>
+  )
   return (
-    
+    <div>SearchResults</div>
   )
 }
