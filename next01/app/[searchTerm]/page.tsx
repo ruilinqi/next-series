111f800d5ad01653dbe220e2814fe7d4472f25ad
ruilinqi <ruilinqi7@gmail.com>
2023-03-31 00:04:12 -0400

    Generate metadata for a search term by querying Wikipedia and display the search results

--- next01/app/[searchTerm]/page.tsx
+++ next01/app/[searchTerm]/page.tsx
@@ -5,6 +5,27 @@ type Props = {
     searchTerm: string
   }
 }
+
+// generates metadata for a search term by querying Wikipedia and returning a title and description for the search results.
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+  const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
+  const data = await wikiData
+  const displayTerm = searchTerm.replaceAll('%20', ' ')
+
+  // if no data
+  if (!data?.query?.pages) {
+      return {
+          title: `${displayTerm} Not Found`
+      }
+  }
+
+  // if we get data
+  return {
+      title: displayTerm,
+      description: `Search results for ${displayTerm}`
+  }
+}
+
 export default async function SearchResults({params: {searchTerm}}: Props) {
   const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
   const data = await wikiData
